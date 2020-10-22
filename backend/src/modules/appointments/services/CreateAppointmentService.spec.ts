87fc718230c248ba91d5e137ae6ee13ec6a70030
Marcos Paulo Borges <eumarcosborgs@gmail.com>
2020-10-22 12:36:06 -0400

    update test CreateAppointmentService

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,43 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(2020, 4, 10, 13),
+      provider_id: 'provider_id',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 13);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: 'provider_id',
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: 'provider_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
